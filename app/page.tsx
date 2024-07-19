081c4dd6888ad8fe2cc27214fe0821598fd2f639
Jason Rathgeber <jrathgeber@yahoo.com>
2024-07-19 15:12:26 -0400

    tweaked the ui

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,11 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <a href="https://www.jasonrathgeber.com"
+          className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Developed by &nbsp;
           <code className="font-mono font-bold">Jason Rathgeber</code>
-        </p>
+        </a>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
@@ -47,30 +48,30 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            CLOs{" "}
+            Spring-AI{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Chat with CLOs and other financial statements. See AI features in action. 
+            Chat with CLOs and other financial statements. See Spring AI features in action like output parsers and functions. Uses Vaadin Hilla. 
           </p>
         </a>
 
         <a
-          href="https://nice-moss-0cfda6e0f.azurestaticapps.net/"
+          href="https://mechanizd.vercel.app/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Screener{" "}
+            LangChain{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Try the Mechanizd stock and crypto screener. 
+            See LangChain features in action. Comming soon ! 
           </p>
         </a>
 
@@ -81,13 +82,13 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Chatbots{" "}
+            NextJS{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Chat with the stock market and produce research reoports on the fly. 
+            Next JS and AI sample app. Chat with the stock market and produce research reoports on the fly. 
           </p>
         </a>
 
@@ -104,7 +105,7 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            See the Mechanizd performance in the Numerai machine learning tournament.
+            See the Mechanizd performance in the Numerai machine learning tournament. LightGBM and XGBoost models. 
           </p>
         </a>
       </div>
